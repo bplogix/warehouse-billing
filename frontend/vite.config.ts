4dd3c3c2c46d6b4ba5118340bea92f0bd2823013
劉超 <liu.chao@jcbc.co.jp>
2025-12-04 09:25:41 +0900

    配置构建

---
+++ frontend/vite.config.ts
@@ -0,0 +1,19 @@
+import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+import { defineConfig } from 'vite'
+
+// https://vite.dev/config/
+export default defineConfig(() => {
+  const base = process.env.VITE_BASE_PATH || '/'
+
+  return {
+    base,
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+      },
+    },
+  }
+})
