ee64d914ce49cef8e9f00debc06eb999d3dbb81d
劉超 <tamaosamu.jp@gmail.com>
2025-12-04 17:37:34 +0900

    引入 tailwind 和 shadcnUI组件

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,3 +1,4 @@
+import "@/index.css"
 import '@fontsource/roboto/300.css'
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/500.css'
@@ -5,7 +6,6 @@ import '@fontsource/roboto/700.css'
 import { CssBaseline, ThemeProvider, createTheme } from '@mui/material'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-
 import App from './App'
 
 const rootElement = document.getElementById('root')
